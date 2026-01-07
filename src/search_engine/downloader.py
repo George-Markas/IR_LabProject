3fbdeb33371b720639d769b4696e8b2b4a1df25a
George-Markas <georgemarkanton@proton.me>
2026-01-07 15:44:32 +0200

    Added kagglehub download for CISI

--- src/search_engine/downloader.py
+++ src/search_engine/downloader.py
@@ -1,16 +1,21 @@
 import nltk
+import kagglehub
 from nltk.corpus import reuters
 from pathlib import Path
 
-DATA_DIR = Path.joinpath(Path(__file__).parent.parent.parent, "./nltk_data")
+nltk_dir = Path.joinpath(Path(__file__).parent.parent.parent, "./nltk_data")
 
-nltk.data.path.append(DATA_DIR)
+nltk.data.path.append(nltk_dir)
 
 def download_datasets():
+    # Download Reuters
     try:
         reuters.fileids()
     except LookupError:
-        nltk.download('reuters', download_dir=DATA_DIR)
-        nltk.download('punkt', download_dir=DATA_DIR)
-        nltk.download('stopwords', download_dir=DATA_DIR)
-        nltk.download('punkt_tab', download_dir=DATA_DIR)
+        nltk.download('reuters', download_dir=nltk_dir)
+        nltk.download('punkt', download_dir=nltk_dir)
+        nltk.download('stopwords', download_dir=nltk_dir)
+        nltk.download('punkt_tab', download_dir=nltk_dir)
+
+    # Download CISI
+    kagglehub.dataset_download("dmaso01dsta/cisi-a-dataset-for-information-retrieval")
