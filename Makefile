30ee5e67788df2ca6fa61c813633f6db2bd11cef
George-Markas <georgemarkanton@proton.me>
2026-01-07 16:03:45 +0200

    Simplified downloader.py

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ all: run
 venv:
 	python3 -m venv venv
 	./venv/bin/pip install -e .
-	@echo "Setup complete!"
+	@echo "Setup complete"
 
 run: venv
 	./venv/bin/search-engine-cli
@@ -12,6 +12,6 @@ clean:
 	rm -rf venv
 	rm -rf src/*.egg-info
 	rm -rf src/search_engine/__pycache__
-	rm -rf nltk_data
+	@echo "The datasets still have to be removed manually. Check nltk and kagglehub default directories"
 
 .PHONY: setup run clean
--- src/search_engine/downloader.py
+++ src/search_engine/downloader.py
@@ -1,21 +1,17 @@
 import nltk
 import kagglehub
 from nltk.corpus import reuters
-from pathlib import Path
-
-nltk_dir = Path.joinpath(Path(__file__).parent.parent.parent, "./nltk_data")
-
-nltk.data.path.append(nltk_dir)
 
 def download_datasets():
+    # Download CISI
+    path = kagglehub.dataset_download("dmaso01dsta/cisi-a-dataset-for-information-retrieval")
+    print(f"Downloaded CISI dataset to {path}\n")
+
     # Download Reuters
     try:
         reuters.fileids()
     except LookupError:
-        nltk.download('reuters', download_dir=nltk_dir)
-        nltk.download('punkt', download_dir=nltk_dir)
-        nltk.download('stopwords', download_dir=nltk_dir)
-        nltk.download('punkt_tab', download_dir=nltk_dir)
-
-    # Download CISI
-    kagglehub.dataset_download("dmaso01dsta/cisi-a-dataset-for-information-retrieval")
+        nltk.download('reuters')
+        nltk.download('punkt')
+        nltk.download('stopwords')
+        nltk.download('punkt_tab')
